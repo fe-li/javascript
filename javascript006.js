f90be2d06ece4b9bb2b282f05b592f979a9ec40e
fe-li <kamil.feliszewski@outlook.com>
2018-04-08 19:27:12 +0200

    switch

---
+++ javascript006.js
@@ -0,0 +1,20 @@
+var a=20;
+
+var b=7;
+
+switch(a * b){
+    case 10:
+        document.write("Wynik mnożenia wynosi 140");
+        break;
+
+    case 40:
+        document.write("wynik mnożenia wynosi 40");
+        break;
+
+    case 100:
+        document.write("Wynik mnożenia wynosi 100");
+        break;
+
+    default:
+        document.write("Nieznany wynik mnożenia 20x7");
+}
